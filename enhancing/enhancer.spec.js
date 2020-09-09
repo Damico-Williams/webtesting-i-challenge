9d9dc33cfc6dfeca621a92166e5e7e27cb1f79de
Damico Williams <damicowilliams@gmail.com>
2020-09-08 18:20:48 -0700

    working on last test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,29 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+item = {
+    name: "sword",
+    durability: 80,
+    enhancment: 20
+}
+
+describe("testing the enhancer", () => {
+    it("repairs", () => {
+        expect(enhancer.repair(item).durability).toEqual(100)
+    })
+
+    it("succeeds", () => {
+        const newItem = enhancer.success(item)
+        const stockEnhancment = item.enhancment
+
+        if(item.enhancment === 20) {
+            expect(newItem.enhancment).toEqual(stockEnhancment)
+        } else {
+            expect(newItem.enhancment).toEqual(stockEnhancment + 1)
+        }
+    })
+
+    it("fails", () => {
+        expect()
+    })
+})
